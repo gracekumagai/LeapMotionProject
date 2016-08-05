1a2480f06d7246e2d1873de630b3c96eeb4dbfdd
gracekumagai <g.kumagai@mail.utoronto.ca>
2016-08-05 13:53:59 -0400

    Upload files

---
+++ Assets/LeapProject/Scripts/CubeScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        gameObject.GetComponent<Renderer>().material.color = Color.green;
+	}
+
+}
